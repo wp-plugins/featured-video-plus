89cfc8041fe086ea5394c3fe268e4467ec25d4d2
a.hoereth <a.hoereth@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-01 19:16:35 +0000

    Commit version 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/featured-video-plus/trunk@1172119 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/frontend.js
+++ js/frontend.js
@@ -1,97 +1,195 @@
-/**
- * Used for removing anchors surrounding featured videos
- *
- * @since 1.8
- */
-function fvp_unwrap() {
-	jQuery('.featured_video_plus, a.fvp_overlay, a.fvp_dynamic').each(function() {
-		if( jQuery(this).parent().is('a') )
-			jQuery(this).unwrap();
-	});
-}
+(function($) {
+  'use strict';
+  /* global fvpdata */
 
-/**
- * Replace featured image with the featured video on click
- *
- * @since 1.7
- */
-function fvp_dynamic(id){
-	var t = jQuery('#fvp_'+id);
-	t.css({'position':'relative'})
-	 .prepend(jQuery('<div />', {
-		'class':'fvp_loader',
-		'style':'background:transparent url(\''+fvpdata.loadingb+'\') no-repeat center center;'+
-						'position: absolute; margin: '+t.children('img').css('margin')+';'+
-						'height:'+t.children('img').css('height')+';width:'+t.children('img').css('width')+'; z-index:1000;'
-	}));
-	jQuery.post( fvpdata.ajaxurl,
-		{
-			'action': 'fvp_get_embed',
-			'nonce' : fvpdata.nonce,
-			'id'    : id
-		},
-	function(data){
-		if (data.success){
-			t.replaceWith(data.html);
-			fvp_unwrap();
-			if ( fvpdata.fitvids && fvpdata.overlay )
-				jQuery(".featured_video_plus").fitVids({customSelector: "iframe[src*='dailymotion.com']"});
-		}
-	});
-}
 
-jQuery(document).ready(function($){
-	// remove wrapping anchors
-	fvp_unwrap();
+  var $loader = $('<div />').addClass('fvp-loader');
+  var playBg = 'url(\'' + fvpdata.playicon + '\')';
+  var loadBg = 'url(\'' + fvpdata.loadicon + '\')';
+  var bgState;
 
-	// add hover effect
-	if( fvpdata.overlay || fvpdata.dynamic ){
-		$('.fvp_overlay,.fvp_dynamic').hover(
-			function(){ $(this).children('img').animate({opacity:0.65});	},
-			function(){ $(this).children('img').animate({opacity:1   }); 	}
-		);
-	}
 
-	// overlay click handler
-	if(fvpdata.overlay){
-	  $('.fvp_overlay').click(function(){
-			$(this).openDOMWindow({
-				eventType:null,
-				windowPadding:0,
-				borderSize:0,
-				windowBGColor:'transparent',
-				overlayOpacity:fvpdata.opacity
-			});
-			$('#DOMWindow').css({'background':"transparent url('"+fvpdata.loadingw+"') center center no-repeat"});
-	  	var id = new RegExp('[\\?&amp;\\#]fvp_([0-9]+)').exec($(this).attr('href'));
-			if ($('#fvp_'+id[1]).html().length === 0){
-				jQuery.post( fvpdata.ajaxurl,
-					{
-						'action': 'fvp_get_embed',
-						'nonce' : fvpdata.nonce,
-						'id'    : id[1]
-					},
-				function(data){
-					if (data.success){
-						// save the data to not reload when opened again
-						//$('#fvp_'+id[1]).html(data.html); // removed due to bugs with Video.js
+  /**
+   * Remove the link wrapping featured images on index pages
+   */
+  function unwrap() {
+    $('.has-post-video a.post-thumbnail>.featured-video-plus,' +
+      '.has-post-video a.post-thumbnail>.fvp-dynamic,' +
+      '.has-post-video a.post-thumbnail>.fvp-overlay,' +
+      '.has-post-video a.post-thumbnail>.mejs-video,' +
+      '.has-post-video a.post-thumbnail>.wp-video'
+    ).unwrap();
+  }
 
-						$('#DOMWindow').html(data.html).css({'width':'auto','height':'auto','margin':'auto auto','overflow':'hidden'});
-						$(window).trigger('scroll');
 
-						if ( fvpdata.videojs )
-							videojs('videojs'+data.id,{'autoplay':true},function(){});
-					}
-				});//.fail(function() { alert("failed here"); });;
-			}else{
-				$('#DOMWindow').html( $('#fvp_'+id[1]).html() ).css({'width':'auto','height':'auto','margin':'auto auto','overflow':'hidden'});
-				$(window).trigger('scroll');
-			}
-		});
-	}
+  /**
+   * Autosize videos using fitvids for responsive videos
+   */
+  function fitVids() {
+    if (fvpdata.fitvids) {
+      $('.featured-video-plus.fvp-responsive').fitVids({
+        customSelector: ['iframe', 'object', 'embed']
+      });
+    }
+  }
 
-	// fitvids
-	if ( fvpdata.fitvids && ! fvpdata.dynamic && ! fvpdata.overlay )
-	  $(".featured_video_plus").fitVids( { customSelector: ["iframe", "object", "embed", ".video-js"] } );
 
-});
+  /**
+   * WordPress forces a maximum size of the global $contentwidth onto local
+   * videos - overwrite it.
+   */
+  function sizeLocal() {
+    if (fvpdata.width && ! fvpdata.fitvids) {
+      $('.fvp-local .wp-video').css({width: fvpdata.width, height: 'auto'});
+      var video = $('.fvp-local .wp-video .wp-video-shortcode');
+      video.attr({
+        width: fvpdata.width,
+        height: (fvpdata.width / video.attr('width') ) * video.attr('heigth')
+      });
+    }
+  }
+
+
+  /**
+   * Trigger the play / load icon (and preload them).
+   */
+  function triggerPlayLoad() {
+    // preload images
+    if (bgState === undefined) {
+      [fvpdata.playicon, fvpdata.loadicon].forEach(function(val) {
+        $('body').append( $('<img/>', { src: val }).hide() );
+      });
+    }
+
+    // trigger image
+    bgState = bgState === playBg ? loadBg : playBg;
+    $loader.css({ backgroundImage: bgState });
+  }
+
+
+  /**
+   * Handle mouseover and mouseout events.
+   */
+  function hover(event) {
+    var $elem = $(event.currentTarget);
+    var $img = $elem.children('img');
+
+    if (0 === $elem.find('.fvp-loader').length) {
+      $img.animate({ opacity: fvpdata.opacity });
+      $elem
+        .css({ position: 'relative' })
+        .prepend(
+          $loader
+            .css({
+              height    :  $img.height(),
+              width     :  $img.width(),
+              marginTop : -$img.height()/2,
+              marginLeft: -$img.width()/2
+            })
+        );
+    } else if (bgState !== loadBg) {
+      $img.animate({ opacity: 1 });
+      $loader.remove();
+    }
+  }
+
+
+  /**
+   * Replace a featured image with its featured video on-click.
+   */
+  function dynamicTrigger(event) {
+    event.preventDefault();
+    var $self = $(event.currentTarget);
+    var id = parseInt($self.attr('data-id'), 10);
+
+    triggerPlayLoad();
+
+    $.post(fvpdata.ajaxurl, {
+      'action': 'fvp_get_embed',
+      'nonce' : fvpdata.nonce,
+      'id'    : id
+    }, function(data){
+      if (data.success) {
+        $self.replaceWith(data.html);
+
+        // Initialize mediaelement.js player for the new videos.
+        $('.wp-audio-shortcode, .wp-video-shortcode').mediaelementplayer();
+
+        // Autosize them if required.
+        fitVids();
+      }
+
+      triggerPlayLoad();
+    });
+  }
+
+
+  /**
+   * Show the overlay on-click.
+   */
+  function overlayTrigger(event) {
+    event.preventDefault();
+    var $self = $(event.currentTarget);
+    var id = parseInt($self.attr('data-id'), 10);
+
+    $self.openDOMWindow({
+      eventType     : null,
+      windowPadding : 0,
+      borderSize    : 0,
+      windowBGColor : 'transparent',
+      overlayOpacity: fvpdata.opacity * 100,
+      width : '100%',
+      height: '100%'
+    });
+
+    $('#DOMWindow').css({ backgroundImage: loadBg });
+
+    var $cache = $('#fvp-cache-' + id);
+
+    // Check if the result is already cached
+    if (0 === $cache.html().length) {
+      $.post(fvpdata.ajaxurl, {
+          'action': 'fvp_get_embed',
+          'nonce' : fvpdata.nonce,
+          'id'    : id
+      }, function(data) {
+        if (data.success) {
+          // cache the result to not reload when opened again
+          $cache.html(data.html);
+
+          $('#DOMWindow').html(data.html);
+          sizeLocal();
+          $(window).trigger('scroll');
+        }
+      });
+    } else {
+      // From cache
+      $('#DOMWindow').html( $cache.html() );
+      $(window).trigger('scroll');
+    }
+  }
+
+
+  // Initialization after DOM is completly loaded.
+  $(document).ready(function() {
+    // remove wrapping anchors
+    // doing this twice with a 1 second delay to fix wrapped local video posters
+    unwrap();
+    setTimeout(unwrap, 1000);
+
+    // initialize fitvids if available
+    fitVids();
+
+    sizeLocal();
+
+    // add hover effect and preload icons
+    $('.fvp-overlay, .fvp-dynamic').hover(hover, hover);
+    triggerPlayLoad();
+
+    // on-demand video insertion click handler
+    $('.fvp-dynamic').click(dynamicTrigger);
+
+    // overlay click handler
+    $('.fvp-overlay').click(overlayTrigger);
+  });
+})(jQuery);
